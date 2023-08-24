0f6a13ed4a7aa2fbc44f15ae16e2196f13ca0834
shaikhasma <shaikh.asma9999@gmail.com>
2023-08-24 10:47:21 +0530

    ERP-16 : Profile Service Structure

---
+++ src/test/java/com/gotech/profileservice/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotech.profileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
